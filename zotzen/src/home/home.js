2bdc0fad2286bda58d3f7b1690d417a26ae1e60a
Laczarade <billjavandel@gmail.com>
2023-11-04 16:36:54 -0700

    Add reminders list

--- zotzen/src/home/home.js
+++ zotzen/src/home/home.js
@@ -1,12 +1,16 @@
 import GoogleCalendar from '../components/calendar/GoogleCalendar';
 import './home.css';
-import ChoreList from '../components/chore-list/chore-list.js'
+import ChoreList from '../components/chore-list/chore-list.js';
+import Reminders from '../components/reminders/reminders.js';
 
 function Home() {
   return (
     <div className="home">
-      <ChoreList></ChoreList>
       <GoogleCalendar></GoogleCalendar>
+      <div className="right-bar">
+        <ChoreList />
+        <Reminders />
+      </div>
     </div>
   );
 }
