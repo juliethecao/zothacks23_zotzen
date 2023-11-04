4e43bbe911ff9d3c737b143f0496392deda344c0
CodingPenguin <nguyenhuydanny@gmail.com>
2023-11-04 17:03:26 -0500

    updating stuff hehe teehee

---
+++ zotzen/src/components/chore-list/chore-list.js
@@ -0,0 +1,4 @@
+const choreList = (
+    <ul>
+    </ul>
+);
--- zotzen/src/components/roommate/roommate.js
+++ zotzen/src/components/roommate/roommate.js
@@ -2,7 +2,17 @@ import './roommate.css';
 
 function Roommate() {
   return (
-    <div></div>
+    <div className = "roommates">
+	<div className = "roommateOne-container">
+    	This is roomie one
+    </div>
+    <div className = "roommateTwo-container">
+    	This is roomie two
+    </div>
+    <div className = "roommateThree-container">
+    	This is roomie three
+    </div>
+</div>
   );
 }
 
